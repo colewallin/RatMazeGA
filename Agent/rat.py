d0c100847dc1cce672ea1ccc27b20d8f37e2cd4e
Cole Wallin <cole.wallin@gmail.com>
2018-04-20 16:15:52 -0500

    Added crossover function

--- Agent/rat.py
+++ Agent/rat.py
@@ -12,12 +12,50 @@ def randomGeneticSequnce():
         dna.append(random.randint(0, 1))
     return dna
 
+# class DNA:
+#     def __init__(self):
+#         self.genes = []
+#         for i in range(LIFE_SPAN):
+#             self.genes.append(random.randint(0, 1))
+
 class Rat:
 
-    def __init__(self):
-        self.dna = randomGeneticSequnce()
+    def __init__(self, dna=None):
+        if dna == None:
+            self.dna = randomGeneticSequnce()
+        else:
+            self.dna = dna
+            
         self.position = MAZE_SIZE/2
-        self.age = 0
+        self.fitness = 0
+
+    def update(self, age):
+        if self.dna[age]:
+            self.moveRight()
+        else:
+            self.moveLeft()
+
+    def display(self):
+        for val in range(1, MAZE_SIZE+1):
+            if val == self.position:
+                print(' R ', end='')
+            elif val == 1:
+                print(' O ', end='')
+            elif val == MAZE_SIZE:
+                print(' C ', end='')
+            else:
+                print(' = ', end='')
+        print()
+        self.printDNA()
+
+    def calculateFitness(self):
+        distanceFromGoal = MAZE_SIZE - self.position
+
+        # It's possible to have a dividing by zero issue here
+        if distanceFromGoal == 0:
+            distanceFromGoal = 0.1
+
+        self.fitness = 1/distanceFromGoal
 
     def getDNA(self):
         return self.dna
@@ -33,23 +71,3 @@ class Rat:
 
     def moveRight(self):
         self.position += 1
-
-    def update(self, age):
-        if self.dna[age]:
-            self.moveRight()
-        else:
-            self.moveLeft()
-
-
-    def display(self):
-        for val in range(1, MAZE_SIZE+1):
-            if val == self.position:
-                print(' R ', end='')
-            elif val == 1:
-                print(' O ', end='')
-            elif val == MAZE_SIZE:
-                print(' C ', end='')
-            else:
-                print(' = ', end='')
-        print()
-        self.printDNA()
--- main.py
+++ main.py
@@ -1,22 +1,64 @@
 import importlib
 import random
+import time
 from Agent.rat import *
 from Maze.maze import *
 
 POPULATION_SIZE = 1
 LIFE_SPAN = 8
 
-def run(population, age):
-    for rat in population.pop:
-        rat.update(age)
-        rat.display()
+def crossover(genes1, genes2):
+    cutpoint = random.randint(1,len(genes1))
+    newDNA = []
+
+    for i in range(len(genes1)):
+        if i < cutpoint:
+            newDNA[i] = genes1[i]
+        else:
+            newDNA[i] = genes2[i]
+
+    return newDNA
+
+def naturalSelection(matingPool, currentPopulation):
+    newPop = Population()
+    for rat in currentPopulation:
+        parentGenes1 = random.choice(matingPool).dna
+        parentGenes2 = random.choice(matingPool).dna
+        childGenes = crossover(parentGenes1, parentGenes2)
+        rat = Rat(childGenes)
 
 class Population:
 
     def __init__(self):
         self.pop = []
+        self.matingPool = []
         for i in range(0, POPULATION_SIZE):
             self.pop.append(Rat())
+
+    def evaluate(self):
+        mostFit = 0
+        for rat in self.pop:
+            rat.calculateFitness()
+
+            # Find the rat with the highest fitness.
+            if rat.fitness > mostFit:
+                mostFit = rat.fitness
+
+        # Normalize the fitness values between 0 and 1
+        for rat in self.pop:
+            rat.fitness = rat.fitness / mostFit
+
+        for rat in self.pop:
+            amount = rat.fitness * 100
+            for i in range(amount):
+                self.matingPool.append(rat)
+
+        self.matingPool = []
+
+    def run(self, age):
+        for rat in self.pop:
+            rat.update(age)
+            rat.display()
 
 def main():
 
@@ -34,8 +76,9 @@ def main():
     ratPop = Population()
 
 
-    for i in range(15):
-        run(ratPop, count)
+    for i in range(50):
+        time.sleep(.1)
+        ratPop.run(count)
         count += 1
         if count == LIFE_SPAN:
             ratPop = Population()

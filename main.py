7e161edfa142f8ec3e429653c5e999809a4da236
Cole Wallin <cole.wallin@gmail.com>
2018-04-30 16:01:13 -0500

    main

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import time
 ### TODO: print average DNA, Average Starting Position, Average Fitness (factor in the weirdness regarding doubling successful rats having 200% fitness),
 
 POPULATION_SIZE = 50
-LIFE_SPAN = 30
-MAX_GENERATIONS = 50
+LIFE_SPAN = 25
+MAX_GENERATIONS = 200
 MAZE_SIZE = 50
 
 
@@ -120,14 +120,14 @@ def main():
     bestRatPerGen = []
     ratPop = Population()
 
-    filename = input("Save results into file (ex. 'stats.csv'): ")
-    file_object = open(filename, 'w+')
+    # filename = input("Save results into file (ex. 'stats.csv'): ")
+    file_object = open("results.csv", 'w+')
     # writing done per most fit rat per generation.
 
     file_object.write("Population Size,Life Span,Max Generations,Maze Size\n")
     file_object.write(str(POPULATION_SIZE) + ',' +  str(LIFE_SPAN) + ',' + str(MAX_GENERATIONS) + ',' + str(MAZE_SIZE) + '\n')
     file_object.write('\n')
-    file_object.write("Current Generation,Best Rat's Starting Position,Best DNA,Best Fitness Score,Best Reached Goal,% Pop Reached Goal\n")
+    file_object.write("Current Generation,Best DNA,Best Fitness Score,Best Reached Goal,% Pop Reached Goal\n")
 
     while generation < MAX_GENERATIONS:
         ratPop.run(count) # run the population number of steps until lifespan of a rat, where lifespan is length of DNA sequence.
@@ -148,7 +148,7 @@ def main():
                     numRatsInPopThatAteCheese+=1
             percentPopAteCheese = (numRatsInPopThatAteCheese / len(ratPop.pop)) * 100
 
-            file_object.write(str(generation + 1) + ',' + str(ratPop.bestRat.startingPosition) + ',' + dnaArrayToString(ratPop.bestRat.dna) + ',' + str(ratPop.bestRat.fitness) + ',' + str(ratPop.bestRat.ateTheCheese) + ',' + str(percentPopAteCheese) + '%' + '\n')
+            file_object.write(str(generation + 1) + ',' + dnaArrayToString(ratPop.bestRat.dna) + ',' + str(ratPop.bestRat.fitness) + ',' + str(ratPop.bestRat.ateTheCheese) + ',' + str(percentPopAteCheese) + '%' + '\n')
 
             ratPop.naturalSelection()
             count = 0
